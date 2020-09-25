3f676180038e195b0d5c0b47f4429be61c973c59
Internal exception: java.lang.NoClassDefFoundError <dilanolivera12@gmail.com>
2020-09-25 08:27:47 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let num = 0
+input.onButtonPressed(Button.A, function () {
+    num = randint(1, 3)
+    if (num == 1) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    } else if (num == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+})
